d8f3815bd302d0b844d5570b1807110f4a69b92d
Sudhikshan7 <sudhikshanchithramani@gmail.com>
2026-04-29 22:43:00 +1000

    Contact added

--- api.py
+++ api.py
@@ -10,6 +10,16 @@ def home():
 def about():
     return "This is a simple flask app"
 
+@app.route("/contact")
+def contact():
+    return "Contact me at www.user.com"
+
+
+
+
+
     
 if __name__ == "__main__":
     app.run(debug =True )
+
+
